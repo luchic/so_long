cc9bdd6bc200a202a361a5b3b9a80e95e4d76026
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-17 18:33:23 +0100

    Add tools for working with arrays

---
+++ src/tools/array_tools.c
@@ -0,0 +1,42 @@
+#include "internal.h"
+
+char	**init_array(int height, int widht)
+{
+	char	**res;
+	int		i;
+
+	res = (char **)ft_calloc(height, sizeof(char *));
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < height)
+	{
+		res[i] = ft_calloc(widht, sizeof(char *));
+	}
+	return res;
+}
+
+char	**copy_array(char **src, int height, int widht)
+{
+	int		i;
+	int		j;
+	char	**res;
+
+	if (!src)
+		return (NULL);
+	res = init_array(height, widht);
+	if (!res)
+		return (NULL);
+	i = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (j <widht)
+		{
+			res[i][j] = src[i][j];
+			j++;
+		}
+		i++;
+	}
+	return (res);
+}
