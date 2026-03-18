f21fd4f5864757ef38a2751a2f67266655ce9721
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-18 15:14:17 +0100

    Add tool function for iteration by layers

---
+++ src/generate/iterate_layer.c
@@ -0,0 +1,19 @@
+#include "internal.h"
+
+void	sl_iterate_layer(t_game *game, void (*f)(t_game*, int, int))
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < game->map.height)
+	{
+		j = 0;
+		while (j < game->map.width)
+		{
+			f(game, i, j);
+			j++;
+		}
+		i++;
+	}
+}
