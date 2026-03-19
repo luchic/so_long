8910c584857b8a77d350459e68d5472f2df20c5e
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-19 10:16:51 +0100

    Fix spelling mistake in fill_interactive_layer

--- src/generate/fill_interactive_layer.c
+++ src/generate/fill_interactive_layer.c
@@ -6,13 +6,13 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 10:13:34 by nluchini          #+#    #+#             */
-/*   Updated: 2026/03/19 10:13:34 by nluchini         ###   ########.fr       */
+/*   Updated: 2026/03/19 10:16:30 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "internal.h"
 
-static void	set_interatvie_tiles(t_game *game, int y, int x)
+static void	set_interactive_tiles(t_game *game, int y, int x)
 {
 	if (game->map.grid[y][x] == 'C')
 		game->map.interactive_layer[y][x] = COLLECTABLE;
@@ -22,5 +22,5 @@ static void	set_interatvie_tiles(t_game *game, int y, int x)
 
 void	sl_fill_interactive_layer(t_game *game)
 {
-	sl_tile_iterate(game, set_interatvie_tiles);
+	sl_tile_iterate(game, set_interactive_tiles);
 }
