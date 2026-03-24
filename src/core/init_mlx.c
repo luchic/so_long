918636ce56d57a1d54d35fbef19e868d9b024276
Nikita Luchinin <nluchini@student.42heilbronn.de>
2026-03-24 13:42:06 +0100

    Fix init loop hook

--- src/core/init_mlx.c
+++ src/core/init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: nluchini <nluchini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 10:14:02 by nluchini          #+#    #+#             */
-/*   Updated: 2026/03/24 13:32:34 by nluchini         ###   ########.fr       */
+/*   Updated: 2026/03/24 13:41:42 by nluchini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 static void	loop_hook(void *data)
 {
 	t_game	*game;
-	double	now;
 
 	game = (t_game *)data;
 	if (game->end_state == TOCLOSE)
@@ -37,7 +36,7 @@ int	sl_init_mlxwindow(t_game *game)
 	{
 		return (0);
 	}
-	mlx_loop_hook(game->mlx, sl_render_animation_frame, game);
+	mlx_loop_hook(game->mlx, loop_hook, game);
 	mlx_key_hook(game->mlx, sl_handle_key, game);
 	mlx_close_hook(game->mlx, sl_close_hook, game);
 	return (1);
